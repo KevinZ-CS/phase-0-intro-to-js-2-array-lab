fdab2bca8666096d6ad0856f4d2b172598641445
Kevin Zheng <kevzheng27@gmail.com>
2022-06-13 18:18:02 -0400

    commit tree 48267272dd0d9b95acf98a9833bc0961d1337a86

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+cats.push(name);}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+} 
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+} 
+
+function appendCat(name) {
+    const newarry = [...cats, name];
+    return newarry;
+}
+
+function prependCat(name) {
+    const newarry = [name, ...cats];
+    return newarry;
+}
+
+function removeLastCat() {
+    const newarry = cats.slice(0, 2);
+    return newarry;
+}
+
+function removeFirstCat() {
+    const newarry = cats.slice(1);
+    return newarry;
+}
